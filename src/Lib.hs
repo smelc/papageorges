33c12274691dc08d62dca734f6b3fc275ddb9f8f
smelc <clement.hurlin@gmail.com>
2020-01-20 14:06:51 +0100

    Towards considering previous assignments

--- src/Lib.hs
+++ src/Lib.hs
@@ -6,6 +6,7 @@
 
 module Lib where
 
+import Control.Monad.Except
 import Control.Monad.State
 import Data.List
 import Debug.Trace
@@ -16,6 +17,7 @@ import System.Random
 -- from the same person
 data PapaState a = PapaState {
     domain :: [a]            -- ^ The list of persons
+    , previous :: [[(a, a)]] -- ^ Past assignments
     , assignment :: [(a, a)] -- ^ The assignment (who gives to who)
     , rng :: StdGen          -- ^ The random number generator
 }
@@ -24,6 +26,11 @@ instance Show a => Show (PapaState a) where
   show PapaState{domain, assignment} = "[" ++ intercalate "," domainStrs ++ "]\n" ++ intercalate "\n" assignStrs
     where domainStrs :: [String] = map show domain
           assignStrs :: [String] = map (\x -> show (fst x) ++ "->" ++ show (snd x)) assignment
+
+sort :: Ord a => PapaState a -> PapaState a
+sort PapaState{domain, previous, assignment, rng} =
+  PapaState (Data.List.sort domain) previous assignment' rng
+  where assignment' = sortBy (\(giver1, _) (giver2, _) -> compare giver1 giver2) assignment
 
 presentLess :: Eq a => PapaState a -- ^ A state
               -> [a]               -- ^ The list of persons that weren't given a present yet
@@ -48,14 +55,50 @@ assign0 state =
 assign :: Eq a => Show a => PapaState a -> PapaState a
 assign = until (null . presentLess) assign0
 
-nbPersons = 5
+data Where = Commercy | George
+
+getPreviousAssignments :: Where -> Either Error [[(String, String)]]
+getPreviousAssignments location =
+  let
+    result = past location
+    lengths :: [Int] = map length result -- the lengths of past assignments, should all be the same
+  in
+    if length lengths > 1 -- TODO validate more stuff
+    then Left InvalidAssignments
+    else Right result
+  where
+    past :: Where -> [[(String, String)]]
+    past Commercy = [
+      [("Clement", "Thomas"), -- 2019
+       ("Henry", " Marianne"),
+       ("Elise", "Pascale"),
+       ("Laura", " Henry"),
+       ("Marianne", "Romain"),
+       ("Pascale", " Laura"),
+       ("Romain", "Clement"),
+       ("Thomas", " Elise")]
+      ]
+    past George = []
+
+data Error = InvalidAssignments
+  deriving Show
+
+getPersons :: Where -> [String]
+getPersons Commercy = ["Elise", "Clement", "Henry", "Pascale", "Marianne",
+                       "Thomas", "Romain", "Laura"]
+getPersons George = []
+
+main0 :: StdGen -> Where -> Either Error (PapaState String)
+main0 g location = do
+  pastAssignments <- getPreviousAssignments location
+  let initialState = PapaState domain pastAssignments [] g
+      result = assign initialState
+  return $ Lib.sort result
+  where
+    domain = getPersons location
 
 entrypoint :: IO ()
 entrypoint = do
-    g <- newStdGen
-    let (v :: Int, g') = randomR (1, 10) g
-        domain = [0 .. nbPersons - 1]
-        initialState = PapaState domain [] g
-        result = assign initialState
-    print result
+    g :: StdGen <- newStdGen
+    print $ main0 g Commercy
     return ()
