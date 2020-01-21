850c4487b6ea8cd2f5273339f917944ee5b85d92
smelc <clement.hurlin@gmail.com>
2020-01-21 12:08:19 +0100

    Move StdGen out from PapaState, using the State monad

--- src/Lib.hs
+++ src/Lib.hs
@@ -7,8 +7,7 @@
 module Lib where
 
 import Control.Exception
-import Control.Monad.Except
-import Control.Monad.State
+import Control.Monad.State.Lazy
 import Data.List
 import Debug.Trace
 import System.Random
@@ -16,12 +15,10 @@ import System.Random
 -- State encoding that a list of person must give a gift to another person
 -- A person cannot give a gift to itself and two persons should not receive a gift
 -- from the same person
--- TODO smelc Use the State monad to move rng out
 data PapaState a = PapaState {
     domain :: [a]            -- ^ The list of persons
     , previous :: [[(a, a)]] -- ^ Past assignments
     , assignment :: [(a, a)] -- ^ The assignment (who gives to who)
-    , rng :: StdGen          -- ^ The random number generator
 }
 
 instance Show a => Show (PapaState a) where
@@ -30,8 +27,8 @@ instance Show a => Show (PapaState a) where
           assignStrs :: [String] = map (\x -> show (fst x) ++ "->" ++ show (snd x)) assignment
 
 sort :: Ord a => PapaState a -> PapaState a
-sort PapaState{domain, previous, assignment, rng} =
-  PapaState (Data.List.sort domain) previous (sortOn fst assignment) rng
+sort PapaState{domain, previous, assignment} =
+  PapaState (Data.List.sort domain) previous (sortOn fst assignment)
 
 presentLess :: Eq a => PapaState a -- ^ A state
               -> [a]               -- ^ The list of persons that weren't given a present yet
@@ -41,21 +38,26 @@ notGiving :: Eq a => PapaState a -- ^ A state
            -> [a]                -- ^ The list of persons that do not give a present yet
 notGiving PapaState{domain, assignment} = [x | x <- domain, x `notElem` map fst assignment]
 
-assign0 :: Eq a => Show a => PapaState a -> PapaState a
-assign0 state =
-  let dice = rng state
-      (giverIndex, dice') = randomR (0, length candidateGivers - 1) dice
+assign0 :: (Eq a, Show a) => PapaState a -> State StdGen (PapaState a)
+assign0 state = do
+  dice <- get
+  let (giverIndex, dice') = randomR (0, length candidateGivers - 1) dice
       assign = assignment state
       assign' = (candidateGivers !! giverIndex, receiver) : assign
-      state'  = state { assignment = assign', rng = dice'}
-      in state'
+      state'  = state { assignment = assign' }
+  put dice'
+  return state'
   where job = presentLess state
         receiver = head job
         allPrevious = concat $ previous state
         candidateGivers = [x | x <- notGiving state, (x, receiver) `notElem` allPrevious]
 
-assign :: Eq a => Show a => PapaState a -> PapaState a
-assign = until (null . presentLess) assign0
+assign :: (Eq a, Show a) => StdGen -> PapaState a -> PapaState a
+assign rng state =
+  if null $ presentLess state
+  then state
+  else let (state', rng') = runState (assign0 state) rng in
+       assign rng' state'
 
 data Where = Commercy | George
 
@@ -87,13 +89,12 @@ getPersons Commercy = ["Elise", "Clement", "Henry", "Pascale", "Marianne",
 getPersons George = []
 
 main0 :: StdGen -> Where -> PapaState String
-main0 g location =
-  let pastAssignments = getPreviousAssignments location
-      initialState = PapaState domain pastAssignments [] g
-      result = assign initialState in
-      Lib.sort result
+main0 rng location =
+  Lib.sort $ assign rng initialState
   where
+    pastAssignments = getPreviousAssignments location
     domain = getPersons location
+    initialState = PapaState domain pastAssignments []
 
 entrypoint :: IO ()
 entrypoint = do
